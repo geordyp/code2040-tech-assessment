8ddf5844f4cd994896d90d1dcb7c86ea47fc6390
geordyp <geowilliams6@gmail.com>
2016-10-04 21:26:16 -0500

    Complete step 2 of tech assessment
    For step 2, I needed to reverse a string. I did this by
    starting with the last character in the string and looping
    backwards concatenating each character onto a new string.

--- TechAssessment.py
+++ TechAssessment.py
@@ -1,14 +1,27 @@
 import json
 import urllib2
 
-# registration endpoint
-targetURL = 'http://challenge.code2040.org/api/register'
+# my API token: 30e2d792d1001d661697aed566e0341a
 
-# the registration endpoint expects a JSON dictionary with two keys, token and github
-registrationKeys = {'token': '30e2d792d1001d661697aed566e0341a',
-                    'github': 'https://github.com/geordyp/code2040-tech-assessment'}
+# this endpoint will return a string that needs to be reverse
+targetURL = 'http://challenge.code2040.org/api/reverse'
+dictionary = {'token': '30e2d792d1001d661697aed566e0341a'}
 
-# http://www.stackoverflow.com/questions/9746303/how-do-i-send-a-post-request-as-a-json
 request = urllib2.Request(targetURL)
 request.add_header('Content-Type', 'application/json')
-response = urllib2.urlopen(request, json.dumps(registrationKeys))
+response = urllib2.urlopen(request, json.dumps(dictionary))
+
+# reverse the input string
+input = response.read(100)
+output = ''
+inputLength = len(input)
+for i in range(0, inputLength):
+    output += input[(inputLength - 1) - i]
+
+# return the resulting string to this url for validation
+targetURL = 'http://challenge.code2040.org/api/reverse/validate'
+dictionary = {'token': '30e2d792d1001d661697aed566e0341a', 'string': output}
+
+request = urllib2.Request(targetURL)
+request.add_header('Content-Type', 'application/json')
+response = urllib2.urlopen(request, json.dumps(dictionary))
